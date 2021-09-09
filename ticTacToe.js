cd272d54ba7ab23901a0e5fc0e65ad3f4957fffa
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-09-09 15:29:11 +0100

    Added functions to gameBoard module to recognise when there are three X's or O's in a row

--- ticTacToe.js
+++ ticTacToe.js
@@ -9,10 +9,48 @@ const gameBoard = (function() {
         square.innerHTML = board[board.length - 1];
     }
 
+    // These functions check if there are three noughts or crosses in a row.
+    const vertWinnerCheck = (mark) => {
+        for (let i = 0; i < 3; i ++){
+            if (Array.from(gameSquares[i].classList).includes(mark) 
+            && Array.from(gameSquares[i + 3].classList).includes(mark) 
+            && Array.from(gameSquares[i + 6].classList).includes(mark)){
+                return mark;
+            }
+        }
+    }
+
+    const horWinnerCheck = (mark) => {
+        for (let i = 0; i < 9; i += 3){
+            if(Array.from(gameSquares[i].classList).includes(mark) 
+            && Array.from(gameSquares[i + 1].classList).includes(mark) 
+            && Array.from(gameSquares[i + 2].classList).includes(mark)){
+                return mark;
+            }
+        }
+    }
+
+    const diagWinnerCheck = (mark) => {
+        if (Array.from(gameSquares[0].classList).includes(mark) 
+        && Array.from(gameSquares[4].classList).includes(mark) 
+        && Array.from(gameSquares[8].classList).includes(mark)){
+            return mark;
+        }
+
+        else if (Array.from(gameSquares[2].classList).includes(mark) 
+        && Array.from(gameSquares[4].classList).includes(mark) 
+        && Array.from(gameSquares[6].classList).inlcudes(mark)){
+            return mark;
+        } 
+    }
+
     return {
         render,
         board,
         gameSquares,
+        vertWinnerCheck,
+        horWinnerCheck,
+        diagWinnerCheck,
     };
 })();
 
@@ -22,7 +60,10 @@ const Player = (mark) => {
     
     let round = 1;
 
-    const _roundCount = () => round++;
+    const _roundCount = () => {
+        round++;
+        return round;
+    }
 
     const _playRound = (square, opponent) => {
         switch(round % 2 === 1){
@@ -42,10 +83,15 @@ const Player = (mark) => {
     const playerMove = (opponent) => {
         gameBoard.gameSquares.forEach(square => {
             square.addEventListener('click', () => {
-                if (!Array.from(square.classList).includes('cross') && !Array.from(square.classList).includes('nought')){
+                if (!Array.from(square.classList).includes('cross') && !Array.from(square.classList).includes('nought') && round <=9){
                     _playRound(square, opponent);
                     _roundCount();
+                    console.log(gameBoard.gameSquares);
+                    console.log(gameBoard.diagWinnerCheck('cross'));
+                    console.log(gameBoard.diagWinnerCheck('nought'));
                 }
+
+                if (round === 10) displayController.drawDeclaration();  
             })
         })
     }
@@ -54,23 +100,25 @@ const Player = (mark) => {
         playerMove,
         round,
         getMark,
+        _roundCount,
     }
 }
 
-const player1 = Player('cross');
-const player2 = Player('nought');
-
 const displayController = (() => {
 
-    const simulateGame = () => {
+    const drawDeclaration = () => {
+        const winner = document.querySelector('.winner');
+        winner.textContent = "It is a draw!";
     }
 
-    return {
-        simulateGame,
+    return{
+        drawDeclaration,
     }
+
 })();
 
+const player1 = Player('cross');
+const player2 = Player('nought');   
+
 player1.playerMove(player2);
-
-
 
